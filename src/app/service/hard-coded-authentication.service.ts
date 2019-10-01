a7fbe86e96779defd358782655e3a52fb769d4bd
Wageesha95 <imwageesha@gmail.com>
2019-10-01 19:45:38 +0530

    navbar tabs working according to  session storage, when not login no inside tabs showing

--- src/app/service/hard-coded-authentication.service.ts
+++ src/app/service/hard-coded-authentication.service.ts
@@ -8,10 +8,21 @@ export class HardCodedAuthenticationService {
   constructor() { }
 
 authenticate(userName,password){
+
+  console.log( "before :" + this.isUserLogedIn());
   if(userName === "Thilina" && password === '0000'){
+    sessionStorage.setItem('authenticateUser',userName)
+    console.log("after :" + this.isUserLogedIn());
     return true
   }
   return false
+  
+}
+
+isUserLogedIn(){
+  let user = sessionStorage.getItem('authenticateUser')
+  return !(user===null)
 }
 
 }
+
