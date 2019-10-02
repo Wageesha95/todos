20aac552d11597c13df514339c590108c372062e
Wageesha95 <imwageesha@gmail.com>
2019-10-02 15:55:41 +0530

    end of the first frontend development chapter

--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { WelcomeDataService } from '../service/data/welcome-data.service';
 
 @Component({
   selector: 'app-welcome',
@@ -11,13 +12,18 @@ export class WelcomeComponent implements OnInit {
   Msg = 'Some Welcome Message'
   name= ''
 
-  constructor(private route : ActivatedRoute) { }
+  constructor(private route : ActivatedRoute, private service : WelcomeDataService) { }
 
   ngOnInit() {
 
     console.log(this.Msg)
     this.name = this.route.snapshot.params['name']
 
+  }
+
+  getWelcomeMessage(){
+    this.service.executeHelloWorldBeanService();
+    
   }
 
 
